b9dccf7ae129cc87541a5341185d1672eddd0a38
Rahmat Fajar Saputra <fajar.saputra2907@gmail.com>
2025-06-19 06:53:53 +0700

    hehe

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HeroSection from './pages/HeroSection';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Website Informasi Beasiswa</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" component={HeroSection} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;

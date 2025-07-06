74cd3f146835c909794ec1251df579dc6e5e7cae
Rahmat Fajar Saputra <fajar.saputra2907@gmail.com>
2025-07-06 19:04:51 +0700

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import Swal from 'sweetalert2'
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
-function Navbar() {
+function SidebarAdmin() {
     const { isAuthenticated, userRole, logout } = useAuth();
     const navigate = useNavigate();
 
@@ -19,20 +19,96 @@ function Navbar() {
     }
 
     return (
-        <div className="navbar bg-primary shadow-sm">
-            <div className="flex-1">
-                <Link to="/" className="btn btn-ghost text-xl">ScholarMatch</Link>
-            </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li><Link to="/">Beranda</Link></li>
-                    {isAuthenticated && userRole === 'user' && (
-                        <li><Link to="/list-beasiswa">Beasiswa</Link></li>
-                    )}
-                    {isAuthenticated && userRole === 'admin' && (
-                        <li><Link to="/beranda-admin">Admin</Link></li>
-                    )}
+        <div className="drawer">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+                {/* Navbar */}
+                <div className="navbar bg-primary w-full">
+                    <div className="flex-none lg:hidden">
+                        <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                className="inline-block h-6 w-6 stroke-current"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                ></path>
+                            </svg>
+                        </label>
+                    </div>
+                    <div className="flex-1">
+                        <Link to="/" className="btn btn-ghost text-xl">ScholarMatch</Link>
+                    </div>
+                    <div className="flex-none">
+                        <ul className="menu menu-horizontal px-1">
+                            <li><Link to="/">Beranda</Link></li>
+                            {isAuthenticated && userRole === 'user' && (
+                                <li><Link to="/list-beasiswa">Beasiswa</Link></li>
+                            )}
+                            {isAuthenticated && userRole === 'admin' && (
+                                <li><Link to="/beranda-admin">Admin</Link></li>
+                            )}
 
+                            {!isAuthenticated ? (
+                                <>
+                                    <li><Link to="/login">Login</Link></li>
+                                    <li><Link to="/register">Register</Link></li>
+                                </>
+                            ) : (
+                                <>
+                                    <Menu as="div" className="ml-4 relative inline-block text-left">
+                                        <div>
+                                            <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-primary px-3 py-2 text-white-100 ring-1 ring-gray-300 ring-inset hover:cursor-pointer">
+                                                <li>
+                                                    Hello
+                                                </li>
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                </svg>
+                                                <ChevronDownIcon aria-hidden="true" className="-mr-1 size-5 text-white-400" />
+                                            </MenuButton>
+                                        </div>
+
+                                        <MenuItems
+                                            transition
+                                            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                                        >
+                                            <div className="py-1">
+                                                <MenuItem>
+                                                    <a
+                                                        href="#"
+                                                        className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                                                    >
+                                                        Account settings
+                                                    </a>
+                                                </MenuItem>
+                                                <MenuItem>
+                                                    <button
+                                                        onClick={handleLogout}
+                                                        type="submit"
+                                                        className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden hover:cursor-pointer"
+                                                    >
+                                                        Logout
+                                                    </button>
+                                                </MenuItem>
+                                            </div>
+                                        </MenuItems>
+                                    </Menu>
+                                </>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+                <ul className="menu bg-base-200 min-h-full w-80 p-4">
+                    {/* Sidebar content here */}
                     {!isAuthenticated ? (
                         <>
                             <li><Link to="/login">Login</Link></li>
@@ -40,7 +116,7 @@ function Navbar() {
                         </>
                     ) : (
                         <>
-                            <Menu as="div" className="ml-4 relative inline-block text-left">
+                            <Menu as="div" className="m-4 relative inline-block text-left">
                                 <div>
                                     <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-primary px-3 py-2 text-white-100 ring-1 ring-gray-300 ring-inset hover:cursor-pointer">
                                         <li>
@@ -49,8 +125,7 @@ function Navbar() {
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                         </svg>
-                                        <ChevronDownIcon aria-hidden="true" className="-mr-1 size-5 text-white-400" />
-                                    </MenuButton>
+                                        </MenuButton>
                                 </div>
 
                                 <MenuItems
@@ -80,10 +155,25 @@ function Navbar() {
                             </Menu>
                         </>
                     )}
+                    <li><Link to="/">Beranda</Link></li>
+                    {isAuthenticated && userRole === 'user' && (
+                        <>
+                        <li><Link to="/list-beasiswa">Daftar Beasiswa</Link></li>
+                            <button onClick={handleLogout} className="btn btn-info m-4">Logout</button>
+                        </>
+                        
+                    )}
+                    {isAuthenticated && userRole === 'admin' && (
+                        <>
+                            <li><Link to="/beranda-admin">List Beasiswa</Link></li>
+                            <li><Link to="/add-beasiswa">Tambah Beasiswa</Link></li>
+                            <button onClick={handleLogout} className="btn btn-info m-4">Logout</button>
+                        </>
+                    )}
                 </ul>
             </div>
         </div>
     )
 }
 
-export default Navbar;
+export default SidebarAdmin;
--- src/pages/BerandaAdmin.jsx
+++ src/pages/BerandaAdmin.jsx
@@ -40,8 +40,8 @@ function BerandaAdmin() {
                 }}
             >
                 <div className="hero-overlay"></div>
-                <div className="hero-content text-neutral-content w-full"> {/* Tambahkan w-full */}
-                    <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 w-full"> {/* Sesuaikan padding, tambahkan w-full */}
+                <div className="hero-content text-neutral-content w-full">
+                    <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8 w-full">
 
                         <h1 className="mt-8 mb-8 text-3xl sm:text-4xl lg:text-5xl text-center font-bold">Daftar Beasiswa</h1> {/* Sesuaikan ukuran teks */}
                         <div className="overflow-x-auto rounded-box border border-base-content/5 bg-white bg-opacity-80 p-4 sm:p-6 shadow-lg"> {/* Sesuaikan padding */}
@@ -51,7 +51,6 @@ function BerandaAdmin() {
                                 <Link to="/add-beasiswa" className="btn btn-info btn-md sm:btn-lg">Tambah Beasiswa Baru</Link>
                             </div>
 
-                            {/* Tabel Responsif */}
                             <table className="table w-full mb-6 shadow-md rounded-lg">
                                 <thead className="bg-primary text-white">
                                     <tr>
@@ -68,7 +67,7 @@ function BerandaAdmin() {
                                             <td>{beasiswa.nama}</td>
                                             <td>{beasiswa.jenjang}</td>
                                             <td>{beasiswa.deadline}</td>
-                                            <td className="flex flex-col sm:flex-row gap-2 items-stretch sm:items-center"> {/* Gunakan flexbox untuk aksi tombol */}
+                                            <td className="flex flex-col sm:flex-row gap-2 items-stretch sm:items-center">
                                                 <Link to={`/admin-detail-beasiswa/${beasiswa.id}`} className="btn btn-xs sm:btn-sm btn-primary">Detail</Link>
                                                 <Link to={`/edit-beasiswa/${beasiswa.id}`} className="btn btn-xs sm:btn-sm btn-warning">Edit</Link>
                                                 <button onClick={() => handleDelete(beasiswa.id)} className="btn btn-xs sm:btn-sm btn-error">Hapus</button>
